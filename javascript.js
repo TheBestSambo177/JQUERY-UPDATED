5d25ab19c5578f65abd005cc73a7606ec37a4fb4
Sambo177 <samuel78kennedynz@gmail.com>
2022-10-27 18:17:44 +1300

    work

--- javascript.js
+++ javascript.js
@@ -196,3 +196,16 @@ $('.btn-demo-area button').on('click', function(){
 	//Show only the content area matching to the clicked button
 	$('.content-demo-area div').eq($(this).index()).show(1000);
 });
+
+
+//Upload Image
+const image_input = document.querySelector("#image-input");
+
+image_input.addEventListener("change", function() {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
